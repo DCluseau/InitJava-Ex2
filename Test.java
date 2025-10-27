b924a444a6919bc49c3f6814f3ceff3869e8999a
Daphne Cluseau <daphne.cluseau@fms-ea.com>
2025-10-27 15:28:49 +0100

    Modif pour entrer un String

--- Test.java
+++ Test.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 public class Test{
 	public static void main(String[] args){
 		int nb = 0;
+		String nbin = "";
 		String parite = new String();
 		String positif = new String();
 		// Pour que l'utilisateur puisse entrer le nombre directement en ligne de commande, le programme doit pouvoir récupérer la valeur dans les paramètres du main
 		// Au démarrage du programme, on vérifie si il y a un argument
 		if(args.length > 0){
-			nb = Integer.parseInt(args[0]);
+			nbin = args[0];
 		}else{
 		// Si ce n'est pas le cas, on demande un nombre à l'utilisateur
 			System.out.println("Veuillez entrer un nombre entier : ");
 			Scanner scan = new Scanner(System.in);
-			nb = scan.nextInt();
+			nbin = scan.nextLine();
 		}
-		
+		nb = Integer.parseInt(nbin);
 		if(nb > 0){
 			positif = "positif";
 		}else if(nb < 0) {
@@ -34,12 +35,11 @@ public class Test{
 
 // Si on entre une valeur inattendue, une erreur se produit :
 /*" 
-Exception in thread "main" java.util.InputMismatchException
-        at java.base/java.util.Scanner.throwFor(Scanner.java:977)
-        at java.base/java.util.Scanner.next(Scanner.java:1632)
-        at java.base/java.util.Scanner.nextInt(Scanner.java:2297)
-        at java.base/java.util.Scanner.nextInt(Scanner.java:2251)
-        at Test.main(Test.java:7)
+Exception in thread "main" java.lang.NumberFormatException: For input string: "f"
+        at java.base/java.lang.NumberFormatException.forInputString(NumberFormatException.java:67)
+        at java.base/java.lang.Integer.parseInt(Integer.java:565)
+        at java.base/java.lang.Integer.parseInt(Integer.java:662)
+        at Test.main(Test.java:19)
 "
-Dans ce cas-ci, le programme attend un int mais on a entré un String. Pour régler ce problème, il faudrait utiliser un try...catch et redemander le nombre jusqu'à ce qu'une valeur correcte soit entrée.
+Dans ce cas-ci, le programme attend un numérique mais on a entré une lettre. Pour régler ce problème, il faudrait utiliser un try...catch et redemander le nombre jusqu'à ce qu'une valeur correcte soit entrée. Si on ne peut pas utiliser un try catch, on peut faire en sorte que le programme teste si le String est numérique. Si oui, on continue le programme, sinon, on redemande d'entrer un nombre.
 */
